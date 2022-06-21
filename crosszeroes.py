d4dd9a1a754defe24d2f72f8020a1b04cf282c5c
sanchezz333 <djsan@bk.ru>
2022-06-21 10:56:52 +0300

    strart crosszeroes game

---
+++ crosszeroes.py
@@ -0,0 +1,24 @@
+class Player:
+    """
+    Класс игрока, содержит тип значков и имя.
+    """
+    pass
+
+
+class GameRoundManager:
+    """
+    Менеджер игры, запускающий все процессы.
+    """
+    pass
+
+
+class GameField:
+    pass
+
+
+class GameFieldView:
+    pass
+
+
+class GameWindow:
+    pass
